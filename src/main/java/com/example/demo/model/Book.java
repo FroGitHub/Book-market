b907d00238ab8f7068ac35ac70b71a61d23aa846
FroGitHub <andrey8102310@gmail.com>
2025-03-17 23:58:48 +0200

    fix table name

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -42,7 +42,7 @@ public class Book {
     private boolean isDeleted = false;
     @ManyToMany
     @JoinTable(
-            name = "book_categories",
+            name = "books_categories",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
